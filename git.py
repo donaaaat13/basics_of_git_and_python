6b25ce60d18f42986f69b84a7db48e3ba306961c
Donat Kovats <donat685@gmail.com>
2021-02-07 23:44:31 +0100

    beginners tutorial first day

--- git.py
+++ git.py
@@ -17,8 +17,53 @@ print(ceil(number))
 def mathing():
     m1 = input("Enter a number: ")
     m2 = input("Enter another number: ")
-    result = int(m1)+int(m2)
+    result = float(m1)+float(m2)
     print(result)
     
-mathing()
+# mathing()
+
+dogs = ["Bodri", "Foltos", "Koromos", "Vilmos"] #lists
+nums = [2, 6, 8, 33, 24, 56, 5]
+nums2 = [[3, 6], [3, 6]]
+nums.sort()
+print(dogs[2:])
+print(nums)
+print (nums2[1])
+nums2[1] = 5
+print(nums2)
+
+tuplenums = (2, 6, 8, 33, 24, 56, 5) #tuple, so you cant change
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February"
+}
+
+print(monthConversions["Feb"])
+print(monthConversions.get("Feb"))
+print(monthConversions.get("Bla"))
+
+list2d = [
+    [1, 4, 6, 6],
+    [1, 4, 6],
+    [1, 2, 6, 4, 45]
+    ]
+
+print(list2d[2][4])
+
+for row in list2d:
+    for col in row:
+        print(col)
+
+
+try:
+    value = 10/2
+    print(value)
+except ZeroDivisionError as err1:
+    print(err1)
+except ValueError as err2:
+    print(err2)
+
+
+
 
