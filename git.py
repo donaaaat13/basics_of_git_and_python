934b068d056da7ec5a491e58212c0c10b5bd59d6
Donat Kovats <donat685@gmail.com>
2021-02-07 16:09:02 +0100

    first steps

--- git.py
+++ git.py
@@ -5,7 +5,20 @@ Created on Sun Feb  7 14:07:36 2021
 @author: Donat
 """
 
-x = 1
-if x == 1:
-    # indented four spaces
-    print("x is 1.")
+phrase = "python test"
+print(phrase.replace("test", "fest").upper().isupper())
+
+from math import *
+
+number = 4.7
+print(floor(number))
+print(ceil(number))
+
+def mathing():
+    m1 = input("Enter a number: ")
+    m2 = input("Enter another number: ")
+    result = int(m1)+int(m2)
+    print(result)
+    
+mathing()
+
